50146d28869e05d62856a380cbab472dca868791
FacuCasado <facufcasado@gmail.com>
2023-09-19 20:44:35 -0300

    Routes

--- database/src/server.js
+++ database/src/server.js
@@ -1,11 +1,25 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const router = require('./routes/index')
 
 
 const server = express();
 
 server.use(express.json());
 server.use(morgan('dev'))
+
+server.use('/',router)
+
+server.use('*',(req,res)=>{
+    res.status(404).send('Not Found')
+})
+
+server.use((err,req,res,next)=>{
+    res.status(err.statusCode || 500).json({
+        error:true,
+        message:err.message
+    })
+})
 
 module.exports=server;
