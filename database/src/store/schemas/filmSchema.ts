61cdb530d1a4fde07536fed72d80aacc468157fd
FacuCasado <facufcasado@gmail.com>
2023-09-21 17:54:02 -0300

    film schema

--- database/src/store/schemas/filmSchema.ts
+++ database/src/store/schemas/filmSchema.ts
@@ -1,10 +1,20 @@
 import { Schema } from "mongoose";
+import { IFilm } from "./interface";
 
-const filmSchema = new Schema({
-    _id: String,
-    title: String,
+const filmSchema:Schema = new Schema<IFilm>({
+    _id:{
+        type:String,
+        required:true
+    },
+    title:{
+        type:String,
+        required:true
+    },
     opening_crawl: String,
-    director: String,
+    director:{
+        type:String,
+        required:true
+    },
     producer: String,
     release_date: Date,
     characters: [{
@@ -16,5 +26,21 @@ const filmSchema = new Schema({
         ref:"Planet"
     }]
 })
+
+filmSchema.statics.list = async function (): Promise<IFilm[]>{
+    return await this.find()
+    .populate("characters",["_id","name"])
+    .populate("planets",["_id","name"])
+}
+
+filmSchema.statics.get = async function(_id:number):Promise<IFilm[]>{
+    return await this.findById(_id.toString())
+    .populate("characters",["_id","name"])
+    .populate("planets",["_id","name"])
+}
+
+filmSchema.statics.insert = async function(film:IFilm):Promise<IFilm[]>{
+    return await this.create(film)
+}
 
 export default filmSchema;
