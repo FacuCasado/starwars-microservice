23f6012229324edeb8c01e1e7173d6c7d6dcfb07
FacuCasado <facufcasado@gmail.com>
2023-09-21 18:36:38 -0300

    Interface

--- database/src/store/schemas/interface.ts
+++ database/src/store/schemas/interface.ts
@@ -4,8 +4,8 @@ import { Document, Model } from "mongoose";
 //! Character Interface
 
 export interface ICharacter extends Document{
-    _id?:string,
-    name?:string,
+    _id:string,
+    name:string,
     height?:number,
     mass?:number,
     hair_color?:string,
@@ -25,3 +25,41 @@ export interface ICharacterStatics extends Model<ICharacter>{
 
 //! Film Interface
 
+export interface IFilm extends Document{
+    _id:string,
+    title:string,
+    opening_crawl?:string,
+    director:string,
+    producer?:string,
+    release_date?:Date,
+    characters?:string[]
+    planets?:string[],
+}
+
+export interface IFilmStatics extends Model<IFilm>{
+    list(): Promise<IFilm[]>,
+    get(_id:number): Promise<IFilm[]>,
+    insert(film:IFilm): Promise<IFilm[]>
+}
+
+//! Planet Interface
+
+export interface IPlanet extends Document{
+    _id: string,
+    name:string,
+    rotation_period?: number,
+    orbital_period?: number,
+    diameter?: number,
+    climate?: string,
+    gravity?: string,
+    terrain?: string,
+    surface_water?: number,
+    residents?:string[],
+    films?:string[]
+}
+
+export interface IPlanetStatics extends Model<IPlanet>{
+    list(): Promise<IPlanet[]>,
+    get(_id:number): Promise<IPlanet[]>,
+    insert(planet:IPlanet): Promise<IPlanet[]>
+}
