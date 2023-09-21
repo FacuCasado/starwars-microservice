eefb77e7c264e7a971d1b5e91f5ef6e4cdf58b2c
FacuCasado <facufcasado@gmail.com>
2023-09-21 13:27:34 -0300

    planet schema

---
+++ database/src/store/schemas/planetSchema.ts
@@ -0,0 +1,23 @@
+import { Schema } from "mongoose";
+
+const planetSchema = new Schema({
+    _id: String,
+    name:String,
+    rotation_period: Number,
+    orbital_period: Number,
+    diameter: Number,
+    climate: String,
+    gravity: String,
+    terrain: String,
+    surface_water: Number,
+    residents:[{
+        type:String,
+        ref:"Character"
+    }],
+    films:[{
+        type:String,
+        ref:"Film"
+    }]
+})
+
+export default planetSchema;
