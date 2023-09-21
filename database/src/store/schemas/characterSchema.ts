353b4d18d6cb95db287523f45bf5fbea43fb1410
FacuCasado <facufcasado@gmail.com>
2023-09-21 17:39:19 -0300

    character Schema

--- database/src/store/schemas/characterSchema.ts
+++ database/src/store/schemas/characterSchema.ts
@@ -1,17 +1,43 @@
-import { Schema } from "mongoose";
+import { Schema, Document, Model } from "mongoose";
+import { ICharacter } from "./interface";
 
-const characterSchema = new Schema({
-    _id:String,
-    name:String,
-    height:Number,
-    mass: Number,
-    hair_color: String,
-    skin_color: String,
-    eye_color: String,
-    birth_year: String,
+const characterSchema: Schema = new Schema<ICharacter>({
+    _id:{
+        type:String,
+        required:true
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    height:{
+        type:Number,
+        default:0
+    },
+    mass: {
+        type:Number,
+        default:0
+    },
+    hair_color: {
+        type:String,
+        default:"n/a"
+    },
+    skin_color: {
+        type:String,
+        default:"n/a"
+    },
+    eye_color: {
+        type:String,
+        default:"n/a"
+    },
+    birth_year: {
+        type:String,
+        default:"n/a"
+    },
     gender: {
         type: String,
-        enum:["male", "female", "n/a"]
+        enum:["male", "female", "n/a", "hermaphrodite", "none"],
+        default:"n/a"
     },
     homeworld: {
         type: String,
@@ -22,5 +48,22 @@ const characterSchema = new Schema({
         ref: "Film"
     }]
 })
+
+characterSchema.statics.list = async function (): Promise<ICharacter[]>{
+    return await this.find()
+    .populate("homeworld",["_id","name"])
+    .populate("films",["_id","title"])
+}
+
+characterSchema.statics.get = async function(_id:number):Promise<ICharacter[]>{
+    return await this.findById(_id.toString())
+    .populate("homeworld",["_id","name"])
+    .populate("films",["_id","title"])
+}
+
+characterSchema.statics.insert = async function(character:ICharacter):Promise<ICharacter[]>{
+    return await this.create(character)
+}
+
 
 export default characterSchema;
