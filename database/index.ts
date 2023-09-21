37946174796faf939d23594e8a2e1bce1857200e
FacuCasado <facufcasado@gmail.com>
2023-09-21 17:21:40 -0300

    db connection

--- database/index.ts
+++ database/index.ts
@@ -1,5 +1,11 @@
+import mongoose from 'mongoose';
 import server from './src/server';
-const {PORT}=process.env
+const {PORT, MONGO_URI}=process.env
+
+
+mongoose.connect(MONGO_URI!)
+.then(res=>console.log("Welcome to Mongo!"))
+.catch(error=>console.log(`Mongo error:${error}`))
 
 server.listen(PORT,()=>{
     console.log(`Database microservice listening at port ${PORT}`);
