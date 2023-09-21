61c476c68dcc7d0b7581c6a3756ea066a120d5d7
FacuCasado <facufcasado@gmail.com>
2023-09-21 18:44:45 -0300

    planet schema

--- database/src/store/schemas/planetSchema.ts
+++ database/src/store/schemas/planetSchema.ts
@@ -1,15 +1,37 @@
 import { Schema } from "mongoose";
+import { IPlanet } from "./interface";
 
-const planetSchema = new Schema({
-    _id: String,
-    name:String,
-    rotation_period: Number,
-    orbital_period: Number,
+const planetSchema:Schema = new Schema<IPlanet>({
+    _id:{
+        type:String,
+        required:true
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    rotation_period: {
+        type:Number,
+        default:0
+    },
+    orbital_period: {
+        type:Number,
+        default:0
+    },
     diameter: Number,
-    climate: String,
-    gravity: String,
+    climate: {
+        type:String,
+        default:"n/a"
+    },
+    gravity: {
+        type:String,
+        default:"1 standar"
+    },
     terrain: String,
-    surface_water: Number,
+    surface_water: {
+        type:Number,
+        default:0
+    },
     residents:[{
         type:String,
         ref:"Character"
@@ -19,5 +41,21 @@ const planetSchema = new Schema({
         ref:"Film"
     }]
 })
+
+planetSchema.statics.list = async function (): Promise<IPlanet[]>{
+    return await this.find()
+    .populate("residents",["_id","name"])
+    .populate("films",["_id","title"])
+}
+
+planetSchema.statics.get = async function(_id:number):Promise<IPlanet[]>{
+    return await this.findById(_id.toString())
+    .populate("residents",["_id","name"])
+    .populate("films",["_id","title"])
+}
+
+planetSchema.statics.insert = async function(planet:IPlanet):Promise<IPlanet[]>{
+    return await this.create(planet)
+}
 
 export default planetSchema;
