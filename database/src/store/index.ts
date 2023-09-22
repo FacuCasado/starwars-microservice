00cf8a4bdf01861249e4075e40f4fd6bf9427f7d
FacuCasado <facufcasado@gmail.com>
2023-09-22 20:22:42 -0300

    store

--- database/src/store/index.ts
+++ database/src/store/index.ts
@@ -1,10 +1,23 @@
-import mongoose from "mongoose";
+import mongoose, {Model} from "mongoose";
 import characterSchema from "./schemas/characterSchema";
 import filmSchema from "./schemas/filmSchema";
 import planetSchema from "./schemas/planetSchema";
 import { ICharacter, ICharacterStatics, IFilm, IFilmStatics, IPlanet, IPlanetStatics } from "./schemas/interface";
 
+interface CharacterModel extends Model<ICharacter>, ICharacterStatics {}
+interface FilmModel extends Model<IFilm>, IFilmStatics {}
+interface PlanetModel extends Model<IPlanet>, IPlanetStatics {}
 
-export const Character=mongoose.model<ICharacter, ICharacterStatics>("Character", characterSchema);
-export const Film=mongoose.model<IFilm, IFilmStatics>("Film", filmSchema);
-export const Planet=mongoose.model<IPlanet, IPlanetStatics>("Planet", planetSchema);
+type Store = {
+  Character: CharacterModel;
+  Film: FilmModel;
+  Planet: PlanetModel;
+};
+
+const store:Store={
+    Character:mongoose.model<ICharacter, ICharacterStatics>("Character", characterSchema),
+    Film:mongoose.model<IFilm, IFilmStatics>("Film", filmSchema),
+    Planet:mongoose.model<IPlanet, IPlanetStatics>("Planet", planetSchema)
+}
+
+export default store
