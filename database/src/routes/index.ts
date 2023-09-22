0c3cbfa421e957c4ce2ad76ecb67f01fd2b0e249
FacuCasado <facufcasado@gmail.com>
2023-09-22 20:36:34 -0300

    basic routes

--- database/src/routes/index.ts
+++ database/src/routes/index.ts
@@ -1,6 +1,30 @@
-import {Router} from 'express';
+import {Router, Request, Response} from 'express';
+import store from '../store';
 
 const router=Router()
+
+
+router.get("/:model",async(req:Request,res:Response)=>{
+    const {model}=req.params;
+    const response= await store[model as keyof typeof store].list()
+    res.status(200).json(response)
+})
+
+router.get("/:model/:id",async(req:Request,res:Response)=>{
+    const {model,id}=req.params;
+    const response = await store[model as keyof typeof store].get(id)
+    res.status(200).json(response)
+})
+
+router.post("/:model",async (req:Request, res:Response)=>{
+    const {model}=req.params;
+    const newEntry=req.body;
+
+    const response = await store[model as keyof typeof store].insert(newEntry)
+
+    res.status(201).json(response)
+
+})
 
 
 export default router
