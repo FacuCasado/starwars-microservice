6b88d4df2193bb16ae5c99a2c3af3d8cd4e48d69
FacuCasado <facufcasado@gmail.com>
2023-09-22 22:14:39 -0300

    model validation

--- database/src/middlewares/modelValidation.ts
+++ database/src/middlewares/modelValidation.ts
@@ -1,8 +1,11 @@
 import { Request, Response, NextFunction } from "express"
 import store from "../store";
+import utils from "../utils";
+
+const {DatabaseError}=utils
 
 export default (req:Request,res:Response,next:NextFunction)=>{
     const {model}=req.params;
     if(store.hasOwnProperty(model)) return next()
-    else throw Error('Invalid model')
+    else throw new DatabaseError('Invalid model', 401)
 }
