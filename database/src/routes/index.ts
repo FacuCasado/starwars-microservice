2857f5d6d55bf2e208eaeccfbbbc87617010d364
FacuCasado <facufcasado@gmail.com>
2023-09-22 22:14:47 -0300

    index

--- database/src/routes/index.ts
+++ database/src/routes/index.ts
@@ -1,32 +1,15 @@
-import {Router, Request, Response} from 'express';
-import store from '../store';
+import {Router} from 'express';
 import middlewares from '../middlewares';
+import controllers from '../controllers';
 
 const router=Router()
 
 
-router.get("/:model",middlewares.modelValidation,async(req:Request,res:Response)=>{
-    const {model}=req.params;
-    
-    const response= await store[model as keyof typeof store].list()
-    res.status(200).json(response)
-})
+router.get("/:model",middlewares.modelValidation,controllers.getModel)
 
-router.get("/:model/:id",middlewares.modelValidation,async(req:Request,res:Response)=>{
-    const {model,id}=req.params;
-    const response = await store[model as keyof typeof store].get(id)
-    res.status(200).json(response)
-})
+router.get("/:model/:id",middlewares.modelValidation,controllers.getModelId)
 
-router.post("/:model",middlewares.modelValidation,async (req:Request, res:Response)=>{
-    const {model}=req.params;
-    const newEntry=req.body;
-
-    const response = await store[model as keyof typeof store].insert(newEntry)
-
-    res.status(201).json(response)
-
-})
+router.post("/:model",middlewares.modelValidation,controllers.postModel)
 
 
 export default router
---
+++ database/src/utils/index.ts
@@ -0,0 +1,9 @@
+import catchAsync from "./catchAsync"
+import DatabaseError from "./errors"
+import response from "./response"
+
+export default {
+    catchAsync:catchAsync,
+    DatabaseError:DatabaseError,
+    response:response
+}
