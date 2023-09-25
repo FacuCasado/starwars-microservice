138e3a90d0c9e2b263999055a2596b9f9b3e752e
FacuCasado <facufcasado@gmail.com>
2023-09-25 09:34:04 -0300

    delete by id

--- database/src/controllers/index.ts
+++ database/src/controllers/index.ts
@@ -2,10 +2,12 @@ import getModel from "./getModel";
 import utils from "../utils";
 import getModelId from "./getModelId";
 import postModel from "./postModel";
+import deleteModel from "./deleteModel";
 const {catchAsync}=utils
 
 export default {
     getModel:catchAsync(getModel),
     getModelId:catchAsync(getModelId),
-    postModel:catchAsync(postModel)
+    postModel:catchAsync(postModel),
+    deleteModel:catchAsync(deleteModel)
 }
