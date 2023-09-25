64f612b2fbbec88e67c340b673da99ca515e8cb3
FacuCasado <facufcasado@gmail.com>
2023-09-25 09:52:54 -0300

    delete film

--- database/src/store/schemas/filmSchema.ts
+++ database/src/store/schemas/filmSchema.ts
@@ -1,5 +1,9 @@
 import { Schema } from "mongoose";
 import { IFilm } from "./interface";
+import DatabaseError from "../../utils/errors";
+import store from "..";
+
+const {Character, Planet}=store
 
 const filmSchema:Schema = new Schema<IFilm>({
     _id:{
@@ -44,6 +48,24 @@ filmSchema.statics.get = async function(_id:string):Promise<IFilm[]>{
 
 filmSchema.statics.insert = async function(film:IFilm):Promise<IFilm[]>{
     return await this.create(film)
+}
+
+filmSchema.statics.delete = async function(_id:string):Promise<IFilm[]>{
+    const film = await this.findById(_id)
+
+    if(!film){
+        throw new DatabaseError("Film not found", 404)
+    }else{
+        await Character.updateMany(
+            {_id:{$in:film.characters}},
+            {$pull:{films:_id}}
+        )
+        await Planet.updateMany(
+            {_id:{$in:film.planets}},
+            {$pull:{films:_id}}
+        )
+        return await this.findByIdAndDelete(_id)
+    }
 }
 
 export default filmSchema;
